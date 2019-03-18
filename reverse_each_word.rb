1e123d759e4b4bb62f7c4f88067065a8a13dd2a1
Christopher McIntosh <christopher.l.mcintosh@outlook.com>
2019-03-18 21:40:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   array = string.split(/ /)
   solution = []
-  array.collect do |word|
+  array.each do |word|
     solution << word.reverse
   end
     return solution.join(" ")
